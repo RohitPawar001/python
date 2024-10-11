80064150c9cb53331e0c7248c3f67dbd990d3dad
RohitPawar001 <rppawar491@gmail.com>
2024-10-11 20:36:17 +0530

    typing module

--- Lets_give_the_hints.py
+++ Lets_give_the_hints.py
@@ -19,3 +19,28 @@ for i in range(int(input("enter the no of names to add the list"))):
     
 print(names_list)
 
+
+######## declare the newtype of the variable #########
+from typing import NewType
+
+name_type = NewType("name_type",str) # declared vartype as U
+
+def names(name : name_type) -> name_type:
+    return name
+
+print(names("ram"))
+
+
+############### unpacking ##########
+from typing import TypedDict, Unpack
+
+def movie_rating(Typedict):
+    name:str
+    reting:int
+    
+def rateMovie(**kwargs:Unpack[movie_rating]) -> Unpack[movie_rating]:
+    return kwargs
+
+print(rateMovie(name="a",rating=1))
+
+
